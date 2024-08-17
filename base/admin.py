faa23004849aad4d3059a86a92448709364d1b86
HP Pavilion 10 Gen <HP Pavilion 10 Gen@DONET>
2024-08-18 02:02:33 +0400

    x

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Books, Author, User, Genre, Comment
+from .models import Books, Author, User,Genre, Comment
 # Register your models here.
 admin.site.register(Books)
 admin.site.register(Author)
